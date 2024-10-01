b8ada54b6958d5b4ec2dfbb50c73e9e2122106ae
nishantyaji <nishant.yaji@gmail.com>
2024-10-01 16:50:38 +0530

    Solution of the leetcode problem 1497

--- 1497_CheckIfPairsAreDivisibleByK.kt
+++ 1497_CheckIfPairsAreDivisibleByK.kt
@@ -1,5 +1,6 @@
 fun main() {
     val c = CheckIfPairsAreDivisibleByK()
+    println(c.canArrange(intArrayOf(1, 2, 3, 4, 5, 10, 6, 7, 8, 9), 5))
     println(c.canArrange(intArrayOf(1, 2, 3, 4, 5, 6), 10))
 }
 
@@ -7,26 +8,18 @@ class CheckIfPairsAreDivisibleByK {
 
     fun canArrange(arr: IntArray, k: Int): Boolean {
         val counter = mutableMapOf<Int, Int>()
-        for (a in arr) {
-            counter[((a % k) + k) % k] = counter.getOrDefault(((a % k) + k) % k, 0) + 1
-        }
+        arr.map { a -> counter[((a % k) + k) % k] = counter.getOrDefault(((a % k) + k) % k, 0) + 1 }
         val selfReflectiveIndices = mutableListOf<Int>(0)
         if (k % 2 == 0) {
             selfReflectiveIndices.add((k / 2))
         }
-
-        for (i in 0 until k) {
-            if (i in selfReflectiveIndices) {
-                if (counter.getOrDefault(i, 0) % 2 != 0) {
-                    return false
-                }
-            } else {
-                if (counter.getOrDefault(i, 0) != counter.getOrDefault(k - i, 0)) {
-                    return false
-                }
-            }
-        }
-        return true
+        val refl = selfReflectiveIndices.map { i -> counter.getOrDefault(i, 0) % 2 == 0 }.fold(true) { res, b -> res && b }
+        // can be IntArray(k/2 + 1) below
+        val others = IntArray(k) { it }
+            .filter { i -> i !in selfReflectiveIndices }
+            .map { i -> counter.getOrDefault(i, 0) == counter.getOrDefault(k - i, 0) }
+            .fold(true) { res, b -> res && b }
+        return refl && others
     }
 
 }
