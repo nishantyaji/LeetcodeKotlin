3b2171ed998b23a44f5f06c0f2b05d63167f3323
nishantyaji <nishant.yaji@gmail.com>
2024-11-01 14:23:39 +0530

    Solution to the leetcode problem 1957

---
+++ Problem1957_DeleteCharsToMakeFancyString.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val d = DeleteCharsToMakeFancyString()
+    println(d.makeFancyString("leeetcode"))
+    println(d.makeFancyString("aaabaaaa"))
+    println(d.makeFancyString("aab"))
+}
+
+class DeleteCharsToMakeFancyString {
+    fun makeFancyString(s: String): String {
+//         return s.filterIndexed { index, c -> !(index >= 2 && c == s[index-1] && c == s[index-2]) }.toCharArray().joinToString("")
+         return s.filterIndexed { index, c -> !(index >= 2 && c == s[index-1] && c == s[index-2]) }.toCharArray().joinToString("")
+    }
+}
