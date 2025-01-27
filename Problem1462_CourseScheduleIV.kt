bc6de63022e21f9e2c1e9dd7f8512d02b041240e
nishantyaji <nishant.yaji@gmail.com>
2025-01-27 23:22:04 +0530

    Solution to the leetcode problem 1462

---
+++ Problem1462_CourseScheduleIV.kt
@@ -0,0 +1,20 @@
+class Problem1462_CourseScheduleIV {
+    fun checkIfPrerequisite(numCourses: Int, prerequisites: Array<IntArray>, queries: Array<IntArray>): List<Boolean> {
+        val res = mutableListOf<Boolean>()
+        val dp = Array(numCourses) { Array(numCourses) { false } }
+        for (pre in prerequisites) {
+            dp[pre[0]][pre[1]] = true
+        }
+        for (k in 0 until numCourses) {
+            for (i in 0 until numCourses) {
+                for (j in 0 until numCourses) {
+                    dp[i][j] = dp[i][j] || (dp[i][k] && dp[k][j])
+                }
+            }
+        }
+        for (query in queries) {
+            res.add(dp[query[0]][query[1]])
+        }
+        return res
+    }
+}
